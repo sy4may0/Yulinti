5cd65e5a3d06f83685f9c404e02b3bed440a25b8
sy4may0 <sy4may0@gmail.com>
2026-07-16 00:17:38 +0900

    fix formae applicare time

--- Officia/Ministeria/MinisteriaPuellaeFormae/Ministeria/MinisteriumPuellaeFormae.cs
+++ Officia/Ministeria/MinisteriaPuellaeFormae/Ministeria/MinisteriumPuellaeFormae.cs
@@ -3,7 +3,7 @@ using Yulinti.Officia.Contractus;
 using Yulinti.ImperiumDelegatum.Contractus;
 
 namespace Yulinti.Officia.Ministeria {
-    internal sealed class MinisteriumPuellaeFormae  {
+    internal sealed class MinisteriumPuellaeFormae: IMinisteriumPulsabilisTardus {
         private readonly FormarumPuellae _formarumPuellae;
         private readonly float[] _ratioActualis;
 
@@ -19,23 +19,30 @@ namespace Yulinti.Officia.Ministeria {
 
             int longitudo = Enum.GetValues(typeof(IDPuellaeFormae)).Length;
             _ratioActualis = new float[longitudo];
+
             for (int i = 0; i < longitudo; i++) {
                 if (i == (int)IDPuellaeFormae.Nihil) {
                     continue;
                 }
-                _ratioActualis[i] = -1f;
+                _ratioActualis[i] = 0.5f;
             }
         }
 
         public void Renovare(IDPuellaeFormae forma, float ratio) {
             if (forma == IDPuellaeFormae.Nihil) return;
             _ratioActualis[(int)forma] = ratio;
-            _formarumPuellae.Applicare(forma, ratio);
         }
 
         public float RatioActualis(IDPuellaeFormae forma) {
             if (forma == IDPuellaeFormae.Nihil) return 0.5f;
             return _ratioActualis[(int)forma];
+        }
+
+        public void PulsusTardus() {
+            for (int i = 0; i < _ratioActualis.Length; i++) {
+                if (i == (int)IDPuellaeFormae.Nihil) continue;
+                _formarumPuellae.Applicare((IDPuellaeFormae)i, _ratioActualis[i]);
+            }
         }
     }
 }
